a28385b45858602b1430c47b2ac2b2affab794c8
Ryan Ranjbaran <ryanranjbaran@Ryans-MacBook-Pro.local>
2023-01-18 12:06:56 -0800

    verifyUser controller done

---
+++ server/controllers/userController.js
@@ -0,0 +1,50 @@
+const db =  ; 
+// destructure model to get username and password
+
+const userController = {};
+const bcrypt = require('bcrypt')
+const WORKFACTOR = 12;
+
+usersController.getBcrypt = (req, res, next) => {
+  const pass = req.body.password;
+  bcrypt.hash(pass, WORKFACTOR)
+      .then(hash => {
+          req.body.password = hash;
+          res.locals.user = req.body;
+          return next();
+      })
+}
+
+userController.createUser = async (req, res, next) {
+  
+}
+
+userController.verifyUser = async (req, res, next) => {
+  console.log('req.body', req.body);
+  const { username, password } = req.body;
+  try {
+    const response = await db.findOne({ username: username });   
+    console.log(response);
+    // res.locals.oneUser = response;
+    const databasePw = res.locals.oneUser.password;
+    const verfied = await bcrypt.compare(password, databasePw);
+    if (verfied) {
+      console.log(verfied);
+      return next();
+    } else return res.status(403).json('err in verifyUser controller');
+  }
+  catch (err) {
+    // error 
+  }
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = userController;
--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router(); 
-const dbController = require('../controllers/dbController')
+const dbController = require('../controllers/dbController');
+const userController = require('../controllers/userController');
+const cookieController = require('../controllers/cookieController');
 
 // dbController is an array of all of our controller functions, NOT an object with controller methods
 
 router.get('/', dbController, (req, res) => {
   return res.status(200).json(res.locals.data)
 });
+
+// route to login
+router.get('/login', userController, (req, res) => {
+  return res.status(200).json(res.locals.login);
+})
+
+//route to signup
+router.post('/signup', userController, (req, res) => {
+  return res.status(200).json(res.locals.signup);
+})
 
 module.exports = router;
